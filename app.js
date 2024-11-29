7adf4b77e708f06722e1da8acc9351f85ef46a3d
HahanDmytr <hahandmytro@gmail.com>
2024-11-29 19:34:02 +0100

    add creation router for post

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ require("./db/connectDB");
 
 const express = require("express");
 const auth = require("./routers/auth.js");
+const post = require('./routers/post.js');
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use('/api/v1', auth);
 
+app.use('/api/v1', auth);
+app.use('/api/v2', post);
 
 app.listen(port, () => {
     console.log(`The server running in : ${port}`);
---
+++ routers/post.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Post = require("../models/post.js");
+const Admin = require("../models/Admin.js");
+
+//created posts
+router.post('/addPost', async (req, res) => {
+    try {
+        const { email, title, body } = req.body;
+        const AdminExist = await Admin.findOne({email});
+        if (AdminExist) {
+            const post = new Post({ title, body, author: AdminExist });
+            await post.save().then(() => res.status(200).json({ post }));
+            AdminExist.post.push(post);
+            AdminExist.save();
+            
+        } else {
+            res.status(200).json({message: "Amin is not Exist"});
+        }
+    } catch (error) {
+        res.status(200).json({message: "post didn't save"});
+    }
+    
+});
+
+module.exports = router;
